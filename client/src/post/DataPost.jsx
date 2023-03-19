b1d1f5f1a597faf2e116e34961db9f8f2e0a2ada
Van Tu <levantu.iuh@gmail.com>
2023-03-19 16:49:53 +0700

    role admin

--- client/src/post/DataPost.jsx
+++ client/src/post/DataPost.jsx
@@ -1,4 +1,3 @@
-import { async } from "@firebase/util";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useIsLogin } from "../hooks/useIsLogin";
@@ -7,7 +6,7 @@ import { remove } from "./apiPost";
 
 function DataPost({ post }) {
   const { isLogin } = useIsLogin();
-  const [isAdmin, setIsAdmin] = useState(post.userId == isLogin._id);
+  const [isAdmin, setIsAdmin] = useState(post.userId == isLogin._id || isLogin.isAdmin);
   const handleDel = async () => {
     await remove(post._id, isLogin._id)
     window.location.reload()
@@ -35,7 +34,7 @@ function DataPost({ post }) {
         </div>
         {isAdmin && (
           <>
-            <div onClick={handleDel} className="p-2 rounded-sm text-gray-400 hover:text-pink-500 hover:bg-pink-50 bg-gray-50 cursor-pointer"> <svg width='18px' height='18px' fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" /></svg></div>
+            <div onClick={handleDel} className="p-2 rounded-sm text-gray-400 hover:text-pink-500 hover:bg-pink-50 cursor-pointer"> <svg width='18px' height='18px' fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" /></svg></div>
           </>
         )}
       </div>
